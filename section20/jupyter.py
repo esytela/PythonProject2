fe9f17ee6f695ef5eece4c14ce91b613d3331e35
estela <estelajung0517@gmail.com>
2023-07-02 09:37:09 +0900

    jupyter

---
+++ section20/jupyter.py
@@ -0,0 +1,39 @@
+'''
+jupyter notebook 설치 방법
+    pip installer jupyter
+    (terminal)에서
+
+실행방법
+    jupyter notebook
+
+pycharm 처럼 python 프로그래밍 하는 tool
+webbrowser로 만들어져 있음
+
+바로 바로 코드 확인하기 위해서는 jupyter이 더 좋음. 그치만 여러 파일로 작업할때는 안 좋음
+웹 프로젝트는 여러 파일을 쓰기 때문에 프로젝트 할 때는 pycharm, 데이터 처리는 jupyter
+
+
+명령 모드 (활성화 하려면 ESC키 누름)
+    shift - Enter : 셀 실행, 아래 선택
+    ctrl - Enter : 셀 실행
+    Alt - Enter : 셀 실행, 아래에 삽입
+    y : 코딩
+    m : 마크다운 (파이썬 명령으로 인식 안 함. 문서처럼 글 쓰는거로 인식함)
+        github에 markdown 사용법 찾을 수 있음
+        강사님 github Readme 문서에서 markdown사용법 볼 수 있음
+    a : 위에 셀 삽입
+    b : 아래에 셀 삽입
+    x : 선택한 셀 잘라내기
+    c : 선택한 셀 복사
+    shift - v : 위에 셀 붙여놓기
+    v : 아래에 셀 붙여놓기
+    z : 마지막 셀 삭제 취소
+    d ,d: 선택한 셀 삭제
+    shift-M : 아래 셀 병합
+
+편집 모드 (활성화 하려면 Enter키를 누름)
+    shift- tab : 툴팁표시
+    ctrl-] : 들여쓰기
+    ctrl-shift- : 셀분할
+
+'''
